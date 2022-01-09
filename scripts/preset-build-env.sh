36231111b41b3fdd1971b32eb408603de30b3abe
kingyond <900483@qq.com>
2022-01-09 16:57:31 +0800

    add sudo to run script

--- scripts/preset-build-env.sh
+++ scripts/preset-build-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get --no-install-recommends install -y \
-apt-utils apt-file pkg-config git vim sudo net-tools locales pax \
+apt-get update && apt-get --no-install-recommends install -y \
+apt-utils apt-file pkg-config git vim net-tools locales pax \
 wget curl intltool tree build-essential devscripts ca-certificates python \
 libncurses-dev bc squashfs-tools \
 automake autotools-dev autoconf autopoint autoconf-archive \
@@ -10,8 +10,8 @@ flex nasm cdbs dh-exec quilt mingw-w64 gperf dejagnu lsb-release cpio \
 systemd sharutils time zip xsltproc libxml2-dev zlib1g-dev libselinux1-dev \
 dh-php uuid-dev libblkid-dev libdevmapper-dev libpopt-dev libreadline-dev unzip \
 dos2unix libusb-dev
-sudo apt-get upgrade -y
-sudo dpkg --add-architecture armhf
-sudo apt-get update
-sudo apt-get install -y crossbuild-essential-armhf
-sudo apt-get install -y u-boot-tools libelf-dev
+apt-get upgrade -y
+dpkg --add-architecture armhf
+apt-get update
+apt-get install -y crossbuild-essential-armhf
+apt-get install -y u-boot-tools libelf-dev
