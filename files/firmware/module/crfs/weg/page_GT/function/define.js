9543b8941d3287599f59ed3228a12841328aea88
kingyond <900483@qq.com>
2022-02-12 03:59:13 +0800

    add web function define.js

---
+++ files/firmware/module/crfs/weg/page_GT/function/define.js
@@ -0,0 +1,87 @@
+/*
+SPDX-FileCopyrightText: 2020 Western Digital Corporation or its affiliates.
+
+SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+var DEV_NAME = " [WD My Cloud Mirror ™]";
+var MODEL_NAME = "BWVZ";
+var IPV6_FUNCTION = 0;
+var TUNNEL_BROKER_FUNCTION = 0;
+var BONDING_FUNCTION =0;
+var LAN_PORT_NUM = 1;
+var VOLUME_NUM = 2;
+var ADDON_FUNCTION = 1;
+var VLAN_FUNCTION =0;
+var OLED_FUNCTION =0;
+var POWER_FUNCTION =0;
+var MICROP_FUNCTION = 1;
+var MYCLOUD_FUNCTION =1;
+var SNMP_FUNCTION =0;
+var ADS_FUNCTION =0;
+var SHARE_FUNCTION =1; //facebook,flickr,picasa
+var DFS_FUNCTION =0;			//4-bay:1
+var SAMBA_FUNCTION =0;
+var TMQUOTA_FUNCTION = 0;
+var NFS_FUNCTION =1;
+var LINK_SPEED_FUNCTION =1;
+var JUMBO_FUNCTION =1;
+var AFP_FUNCTION =1;
+var WEBDAV_FUNCTION =0;
+var LLTD_FUNCTION =1;
+var REMOTE_SERVER_FUNCTION =1;
+var ISO_FUNCTION =1;
+var _MAX_TOTAL_AD_ACCOUNT = 800;
+var _MAX_TOTAL_AD_GROUP = 200;
+var _MAX_TOTAL_ACCOUNT = 512;	//4-bay:512
+var _MAX_TOTAL_GROUP = 64;		//4-bay:64
+var _MAX_TOTAL_SHARES = 1024;	//not include created by system,ex:Public,SmartWare,TimeMachineBackup... ITR No.: 99836
+var _MAX_LEN_PW = 64;
+var _MIN_F_TEMP = 140;			//4-bay:140
+var _MAX_F_TEMP = 158;			//4-bay:158
+var _MIN_C_TEMP = 60;			//4-bay:60
+var _MAX_C_TEMP = 70;			//4-bay:70
+var GOOGLEDRIVE_FUNCTION = 0;
+var FLEXIGRID_WIDTH=650;
+var MULTIPLE_USB_FUNCTION = 0;
+var MULTI_LANGUAGE = 0;
+var _MIN_FAN_SPEED = 0;
+var TIME_FORMAT = 12;	//12 or 24
+var DATE_FORMAT = "YYYY-MM-DD";
+var A_UPDATE_URL = "http://products.wdc.com/mycloud/?type=upload&device=mcmg2";
+var A_BACKUP_URL = "http://products.wdc.com/mycloud/?type=backup&device=mcmg2";
+var A_MOBILE_URL = "http://products.wdc.com/mycloud/?type=access&device=mcmg2";
+var A_POLICY_URL = "https://www.westerndigital.com/legal/privacy-statement";
+var D_UPDATE_URL = "http://products.wdc.com/mycloud/?type=uploadlocal&device=mcmg2";
+var D_BACKUP_URL = "http://products.wdc.com/mycloud/?type=backuplocal&device=mcmg2";
+var D_MOBILE_URL = "http://products.wdc.com/mycloud/?type=accesslocal&device=mcmg2";
+var MY_CLOUD_DESKTOP_URL = "http://products.wdc.com/mycloud/?type=desktop&device=mcmg2";
+var FORGOT_PW_URL = "http://www.wd2go.com/forgotPasswordRequest.do";
+var DEV_ID = "wdfmycloud_mirrorg2";
+var PROJECT_NAME = "MyCloudMirrorG2";
+var _ADMIN_UID = "500";
+var _ADMIN_GID = "1001";
+var DDNS_FUNCTION = 1;
+var PORTFORWARDING_FUNCTION = 1;
+var FLASH_LED_FUNCTION = 1
+var SYSTEM_TEST = 1;
+var POWER_SCH_FUNCTION = 1;
+var RECYCLE_BIN_FUNCTION =1;
+var EXTENDED_LOG = 1;
+var DASHBOARD_ACCESS_FUNCTION = 0;
+var WEBHELP_UPDATE = "09012015";
+//Storage
+var STORAGE_RAID_FUNCTION = 1;
+var STORAGE_DISKSTATUS_FUNCTION = 1;
+var STORAGE_ISCSI_FUNCTION =0;
+var STORAGE_VOLVIRTUALIZATION_FUNCTION = 0;
+var VE_FUNCTION = 0;
+var LED_FUNCTION = 1;
+var QUOTA_FUNCTION = 1;
+var MULTI_USER_FUNCTION =1;
+var GROUP_FUNCTION =1;
+var OPLOCKS_FUNCTION =1;
+var NETWORK_UPS_FUNCTION = 1;
+var APP_INSTALL_FUNCTION=1;
+var SHUTDOWN_FUNCTION = 1;
+var REMOTE_DASHBOARD_ACCESS = 1;
